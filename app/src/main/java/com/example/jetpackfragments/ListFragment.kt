afdb4ce810127ebc256b05790731ab09fffe09f3
Fahim Ahmed <p2595607@my365.dmu.ac.uk>
2023-03-21 23:19:58 +0000

    Completed Jetpack Fragments lesson

--- app/src/main/java/com/example/jetpackfragments/ListFragment.kt
+++ app/src/main/java/com/example/jetpackfragments/ListFragment.kt
@@ -1,31 +1,14 @@
 package com.example.jetpackfragments
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 
-/**
- * A simple [Fragment] subclass.
- */
-interface StarSignListener {
-    fun onSelected(starSignId: Int)
-}
 
-class ListFragment : Fragment(), View.OnClickListener {
-
-    private lateinit var starSignListener: StarSignListener
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is StarSignListener) {
-            starSignListener = context
-        } else {
-            throw RuntimeException("Must implement StarSignListener")
-        }
-    }
+class ListFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -53,16 +36,15 @@ class ListFragment : Fragment(), View.OnClickListener {
             view.findViewById(R.id.capricorn)
         )
 
-        starSigns.forEach {
-            it.setOnClickListener(this)
+        starSigns.forEach { starSign ->
+            val fragmentBundle = Bundle()
+            fragmentBundle.putInt(STAR_SIGN_ID, starSign.id)
+            starSign.setOnClickListener(
+                Navigation.createNavigateOnClickListener(
+                    R.id.star_sign_id_action,
+                    fragmentBundle
+                )
+            )
         }
     }
-
-    override fun onClick(view: View?) {
-
-        view?.let { starSign ->
-            starSignListener.onSelected(starSign.id)
-        }
-    }
-
 }
